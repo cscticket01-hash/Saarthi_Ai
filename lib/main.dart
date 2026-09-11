0af65aa7c10b837f155d474e760df7fb5b0d051c
cscticket01-hash <cscticket01@gmail.com>
2026-09-11 11:36:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _SaarthiChatScreenState extends State<SaarthiChatScreen> {
     if (apiKey != null && apiKey.trim().isNotEmpty) {
       setState(() {
         _model = GenerativeModel(
-          model: 'gemini-1.5-flash',
+          model:'gemini-1.5-flash-latest',
           apiKey: apiKey.trim(),
         );
       });
