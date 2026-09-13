2f68d87e33eebe4674c9ebcdc3d17ad3f1796330
cscticket01-hash <cscticket01@gmail.com>
2026-09-13 09:06:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'call_screen.dart';
+import 'main_dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,7 +51,7 @@ home: StreamBuilder<User?>(
             );
           }
           if (snapshot.hasData) {
-            return const ChatScreen();
+            return const MainDashboardScreen();
           }
           return const AuthScreen();
         },
