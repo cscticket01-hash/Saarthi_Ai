43aff9c54885a2be37aedab2f8abeec88686f990
cscticket01-hash <cscticket01@gmail.com>
2026-09-20 19:00:41 +0530

    Update main_dashboard_screen.dart

--- lib/main_dashboard_screen.dart
+++ lib/main_dashboard_screen.dart
@@ -8,6 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:printing/printing.dart';
 
 // ============================================================
 // MAIN DASHBOARD
